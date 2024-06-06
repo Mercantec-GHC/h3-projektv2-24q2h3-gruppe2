9b9d5d94fe2d05eec71692d7586ad014ba2db168
Patrick <patricktl2004@gmail.com>
2024-06-06 08:17:34 +0200

    Removed Backend.sln to fix merge conflicts

--- Backend/API/obj/Release/net6.0/API.AssemblyInfo.cs
+++ Backend/API/obj/Release/net6.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebeac44fe1e4fb556f2f4824608b2777bfcadc7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfbee67b0d8325c044cab65412fe55acadca69f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
